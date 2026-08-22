70cc18387f9095a2839a18de0b92d2b508541a2e
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2026-08-22 05:08:53 +0100

    Bump WorldSatNav version to 1.2.5
    Update main.lua to bump the WorldSatNav version from 1.2.4 to 1.2.5. No other code changes were made; this prepares the project for the corresponding patch release.

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local eventbus = require("WorldSatNav/core/eventbus")
 local WorldSatNav = {
 	name = "WorldSatNav",
 	author = "Madpeter",
-	version = "1.2.4",
+	version = "1.2.5",
 	desc = "Im still not sure where to go"
 }
 
