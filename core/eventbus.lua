fb91c587719870d60b6587a921a9af43c6de4cce
Madpeter <57852827+Madpeterz@users.noreply.github.com>
2026-09-08 14:46:02 +0100

    Remove redundant nil assignments in TriggerEvent
    Delete explicit arg1..arg5 = argN or nil lines in eventbus.TriggerEvent. This cleanup removes unnecessary code because Lua already assigns missing function arguments to nil; behavior remains unchanged.

--- core/eventbus.lua
+++ core/eventbus.lua
@@ -40,11 +40,6 @@ function eventbus.WatchEvent(topic, callback, sourceFile)
 end
 
 function eventbus.TriggerEvent(topic, arg1, arg2, arg3, arg4, arg5)
-    arg1 = arg1 or nil
-    arg2 = arg2 or nil
-    arg3 = arg3 or nil
-    arg4 = arg4 or nil
-    arg5 = arg5 or nil
     if eventtopics.HasTopic(topic) == false then
         return false
     end
